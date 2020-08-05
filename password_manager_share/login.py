9dfb4508904af93dde86f710a39c8825b63f6676
Tigerno1 <40617371+Tigerno1@users.noreply.github.com>
2020-08-05 11:37:09 +1000

    Add files via upload

--- password_manager_share/login.py
+++ password_manager_share/login.py
@@ -1,7 +1,12 @@
 import hashlib 
 import pickle
+
 def secret_password():
+    '''
+    This function is used to generate a password for a user 
+    '''
     while 1: 
+        # input information by user
         name = input('Please put in your name("q" for quit): ')
         if name == 'q':
             break
@@ -10,10 +15,12 @@ def secret_password():
         if password != test:
             print('The password you put in is wrong !')
             continue
-        sha = hashlib.sha1()
+        # sha encrypt
+        sha = hashlib.sha1()             
         sha.update(name.encode('utf-8'))
         sha.update(password.encode('utf-8'))
         manager_pwd = sha.hexdigest()
+        # save file through pickle
         with open('manager_pwd', 'wb') as f:
             pickle.dump(manager_pwd, f)
         break
